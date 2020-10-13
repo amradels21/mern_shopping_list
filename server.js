6386a610145ad421a599192150da22c4b66e47aa
Amr Adel <amradel21@icloud.com>
2020-10-13 12:11:34 +0200

    Version 1.0.2

--- server.js
+++ server.js
@@ -31,9 +31,13 @@ if(process.env.NODE_ENV == 'production'){
     //Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.get('*', function(_, res) {
+        res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+          if (err) {
+            res.status(500).send(err)
+          }
+        })
+      });
 }
 
 const port = process.env.PORT || 5000;
