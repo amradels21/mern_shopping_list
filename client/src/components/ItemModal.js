7d17b9b78682074a40012beae2695b5d154c626f
Amr Adel <amradel21@icloud.com>
2020-10-13 12:00:34 +0200

    Version 1.0.0

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import {Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';    
-import {v1 as uuid} from "uuid"; 
 
 
 
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React , {Component} from 'react';
 import { Container, Button, ListGroup, ListGroupItem} from 'reactstrap';
  import {CSSTransition, TransitionGroup} from 'react-transition-group';
- import {v1 as uuid} from "uuid"; 
 
 import {connect} from 'react-redux';
 import {getItems, deleteItem} from '../actions/itemActions';
